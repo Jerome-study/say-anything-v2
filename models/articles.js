5bff9f6c84ed1b4bfb5b3c6a11934697be079700
jerome.studypurpose@gmail.com <jerome.studypurpose@gmail.com>
2023-07-07 09:03:09 +0800

    changed

--- models/articles.js
+++ models/articles.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
-const {marked} = require('marked')
 const slugify = require('slugify')
-const createDomPurify = require('dompurify')
-const { JSDOM } = require('jsdom')
-const dompurify = createDomPurify(new JSDOM().window)
+
 
 const articleSchema = new mongoose.Schema({
     title: {
@@ -27,10 +24,7 @@ const articleSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    sanitizedHtml: {
-        type: String,
-        required: true
-    }
+    
 })
 
 
@@ -40,14 +34,7 @@ articleSchema.pre('validate', function (next) {
         this.slug = slugify(this.title, { lower: true, strict: true})
     }
 
-    if (this.markDown) {
-        this.sanitizedHtml = dompurify.sanitize(marked(this.markDown))
-       
-    }
-    marked.use({
-        mangle: false,
-        headerIds: false
-    })
+   
 
     
     next()
